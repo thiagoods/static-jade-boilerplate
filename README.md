feb4eb62800afc658ca6cc3be9a148bd5244f116
thiagoods <t.oliveira@outlook.com>
2015-12-16 02:01:05 -0200

    redefinição de template engine utilizado, abandono do jekyll, adoção do jade

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # My-Boilerplate
 Vanilla boilerplate for static sites using jekyll
 ### Inspired and powered by:
-  * [jekyll](https://github.com/jekyll/jekyll)
+  * [jade](https://github.com/pugjs/jade)
   * [web-starter-kit](https://github.com/google/web-starter-kit)
   * [Html5-Boilerplate](https://github.com/h5bp/html5-boilerplate)
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp = require('gulp'),
 		concat = require('gulp-concat'),
 		csslint = require('gulp-csslint'),
 		del = require('del'),
+		jade = require('gulp-jade'),
 		jshint = require('gulp-jshint'),
 		jsreporter = require('jshint-stylish'),
 		imagemin = require('gulp-imagemin'),
@@ -22,97 +23,3 @@ var autoprefixerConfig = {browsers: ['last 2 version', 'ie 9', 'ios 6', 'android
 		stylestatsConfig = require('./.stylestats.json');
 
 /*Tasks*/
-gulp.task('clean', function(cb){
-	del(['dist'], cb)
-});
-
-gulp.task('copy', function(){
-	gulp.src(['src/*.*', '!src/*.html'])
-		.pipe(gulp.dest('dist'));
-});
-
-gulp.task('image', function(){
-	return gulp.src('src/_assets/**/*')
-		.pipe(imagemin({
-			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
-			use: [pngquant({quality: '60-75', speed: 2})]
-		}))
-		.pipe(gulp.dest('dist/assets'))
-});
-
-gulp.task('jekyll', function (cb){
-		var spawn = require('child_process').spawn;
-		var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
-		jekyll.on('exit', function(code) {
-			cb(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+ code);
-		});
-});
-
-gulp.task('html',['jekyll'], function(){
-	return gulp.src('dist/**/*.html')
-		.pipe(gulp.dest('dist'))
-		.pipe(browserSync.reload({stream:true, once: true}))
-});
-
-gulp.task('css', function(){
-	return gulp.src('src/_css/main.scss')
-		.pipe(sass({errLogToConsole: true}))
-		.pipe(autoprefixer(autoprefixerConfig))
-		.pipe(gulp.dest('dist/css'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(minifycss())
-		.pipe(gulp.dest('dist/css'))
-		.pipe(browserSync.reload({stream:true, once: true}))
-});
-
-gulp.task('cssanalyze', function(){
-	return gulp.src('src/_css/main.scss')
-		.pipe(sass({errLogToConsole: true}))
-		.pipe(autoprefixer(autoprefixerConfig))
-		.pipe(csslint(csslintConfig))
-		.pipe(csslint.reporter())
-		.pipe(stylestats({config: stylestatsConfig}))
-});
-
-gulp.task('js', function(){
-	return gulp.src('src/_js/*.js')
-		.pipe(concat('main.js'))
-		.pipe(gulp.dest('dist/js'))
-		.pipe(rename({suffix: '.min'}))
-		.pipe(uglify())
-		.pipe(gulp.dest('dist/js'))
-		.pipe(browserSync.reload({stream:true, once: true}))
-});
-
-gulp.task('jsanalyze', function(){
-	return gulp.src('src/_js/*.js')
-		.pipe(jshint(jshintConfig))
-		.pipe(jshint.reporter('default'))
-		.pipe(jshint.reporter('fail'))
-});
-
-gulp.task('browser-sync', function(){
-	browserSync.init({
-		server: {
-			baseDir: 'dist'
-		}
-	});
-});
-
-gulp.task('analyze', ['build'], function(){
-	gulp.start('cssanalyze', 'jsanalyze');
-});
-
-gulp.task('build', ['clean'], function(){
-	gulp.start('html', 'css', 'js', 'image')
-});
-
-gulp.task('default', ['html', 'css', 'js', 'image'], function(){
-	gulp.watch('src/_posts/*.markdown', ['html']);
-	gulp.watch('src/**/*.html', ['html']);
-	gulp.watch('src/_assets/**/*', ['image']);
-	gulp.watch('src/_css/*.scss', ['css']);
-	gulp.watch('src/_js/*.js', ['js']);
-	gulp.start('browser-sync');
-});
